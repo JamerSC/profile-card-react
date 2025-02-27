195f8473f3163817b05adb0bd29a5ce53bc7fa92
Jamer Catalla <mr.catalla28@gmail.com>
2025-02-27 22:40:21 +0800

    Creating profile card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <h1>Profile Card</h1>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <Intro />
+      <Skilllist skill="Java" />
+      <Skilllist skill="Springboot" />
+      <Skilllist skill="HTML + CSS" />
+      <Skilllist skill="Javascript" />
+      <Skilllist skill="JQuery" />
+      <Skilllist skill="React" />
+      <Skilllist skill="MySQL" />
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div>
+      <img src="/img/jamer.jpg" alt="jamer" className="img" />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h2>Jamer Catalla</h2>
+      <p>
+        Associate Project Manager, Java Springboot & DevOps practitioner, I love
+        reading books, listening news & podcast, learning new things, taking
+        risks, & challenges.
+      </p>
+    </div>
+  );
+}
+
+function Skilllist(props) {
+  return (
+    <div>
+      <span>{props.skill}</span>
+    </div>
+  );
+}
